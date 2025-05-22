6df5880e5364811eb0e1bb6d3c84828e0c4e20e2
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2025-05-22 10:08:16 -0700

    Initial Commit

---
+++ SampleAppForHelpshift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleAppForHelpshift
+//
+//  Created by Tatsuya Moriguchi on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleAppForHelpshift/SampleAppForHelpshiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppForHelpshiftApp.swift
+//  SampleAppForHelpshift
+//
+//  Created by Tatsuya Moriguchi on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppForHelpshiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
